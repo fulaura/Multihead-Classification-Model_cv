efe2771f0dd80af34c9b546118c6d165bdc3f662
Bekzat Ashirbek <91748401+fulaura@users.noreply.github.com>
2026-02-26 14:17:59 +0500

    Delete directory

--- fun/play.py
+++
@@ -1,8 +0,0 @@
-import threading
-import simpleaudio as sa
-
-def play_sound():
-    wave_obj = sa.WaveObject.from_wave_file("alert.wav")
-    wave_obj.play()
-
-threading.Thread(target=play_sound).start()
